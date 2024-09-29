aa74976ae3c747473b4248b8f638b9f0728e8fe8
doubleMajor1120 <gygy991120@gmail.com>
2024-09-29 11:23:50 +0900

    Use Image_generation_pipeline and Turbo

--- image_generation_pipeline.py
+++ image_generation_pipeline.py
@@ -1,43 +1,31 @@
-from diffusers import StableDiffusionXLPipeline
+# flake8: noqa
+
+from diffusers import StableDiffusionXLPipeline, StableDiffusionPipeline
 import torch
 
 class Image_generation_pipeline():
-    def __init__(self, sdxl_model_path, inference_step, textual_inversion_paths, seed):
-        self.inference_step = inference_step
-        self.seed = seed
+    def __init__(self, config, model):
+        self.model = model
+        self.num_inference_step = config['inference_config']['num_inference_step']
+        model_path = config['path'][self.model]
 
-        self.pipe = StableDiffusionXLPipeline.from_pretrained(sdxl_model_path, torch_dtype=torch.float16)
+        if self.model == 'sdxl':
+            self.pipe = StableDiffusionXLPipeline.from_pretrained(model_path, torch_dtype=torch.float16).to("cuda")
+            self.pipe.enable_model_cpu_offload()
+        elif self.model == 'sd1.5':
+            self.pipe = StableDiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16).to("cuda")
+            self.pipe.enable_model_cpu_offload()
+        elif self.model == 'sdxl_turbo':
+            self.pipe = StableDiffusionXLPipeline.from_pretrained(model_path, torch_dtype=torch.float16).to("cuda")
+            self.pipe.enable_model_cpu_offload()
 
-        for path in textual_inversion_paths:
-            self.pipe.load_textual_inversion(path)
-
-    def generate_image(self):
+    def generate_image(self, prompt_list):
         return self.pipe(
-            "futuristic woman in style of Claude Monet's painting", 
-            num_inference_steps=20, 
-            generator=generator,
-            
+            self.style2prompt(prompt_list), 
+            num_inference_steps=self.num_inference_step
         )
 
-    def style2prompt(self, prompt):
+    def style2prompt(self, prompt_list):
         # Here, you'd implement logic to modify the prompt based on the style
-        return prompt.text + " in the style of " + prompt.style
-        
-# Load the Stable Diffusion model pipeline
-pipe = StableDiffusionXLPipeline.from_pretrained("stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16).to("cuda")
-
-# Load different pre-trained Textual Inversion embeddings from Hugging Face
-#pipe.load_textual_inversion("sd-concepts-library/cat-toy")
-#pipe.load_textual_inversion("sd-concepts-library/nebula")
-
-# List of prompts using the textual inversion tokens (e.g., <low-poly>, <arcane>, <moonrise>)
-prompts = [
-    "A portrait of a <cat-toy> warrior",
-    "A woman in <nebula> style"
-]
-
-generator = torch.manual_seed(3)
-# Generate images using different embeddings for each prompt
-images = pipe("futuristic woman in style of Claude Monet's painting", num_inference_steps=20, generator=generator)
-
-image1 = images.images[0]
+        prompt_str_list = [prompt.text_prompt + " in the style of " + prompt.style for prompt in prompt_list] 
+        return prompt_str_list
